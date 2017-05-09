1b5306596f5f2c93d22318e5f486edbe1a6b9f1c
xmg <123456@163.com>
2017-05-09 09:52:05 +0800

    创建模型

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 /**
  * Created by Administrator on 2017/5/9.
  */
+(function (angular) {
+    "use strict";
+    /*创建模块*/
+   var app =  angular.module('app',[]);
+    /*创建控制器  行内式注入*/
+    app.controller('appController',['$scope',function ($scope) {
+        $scope.name =　"今日一刻";
+    }]);
+
+})(angular);
