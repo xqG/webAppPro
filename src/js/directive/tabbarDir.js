6278c4b497276917e62cfd0815561c86af5c4d5f
xmg <123456@163.com>
2017-05-09 10:46:39 +0800

    tabbar指令创建

---
+++ src/js/directive/tabbarDir.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+
+(function (angular) {
+    "use strict";
+    /*创建应用导航指令*/
+    angular.module('app').directive('tabbar',function () {
+        return {
+            restrict:'EA',
+            templateUrl:'../view/tpl/tabbar_tpl.html'
+        }
+    });
+})(angular);
